bcfe9fa07ff4bf1f7d2b75078e31637fc7d3c662
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2023-09-09 21:12:01 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,35 @@
 #!/usr/bin/node
+// script that prints all characters of a Star Wars movie.
+
+const args = process.argv.slice(2);
+if (args < 1) {
+  console.log('Error');
+  process.exit(1);
+}
+
+const episode = args[0];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <n>');
-  process.exit(1);
-}
-
-const id = process.argv[2];
-if (isNaN(id)) {
-  console.log('<n> must be an integer');
-  process.exit(1);
-}
-
-function promiseRequest (url) {
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        reject(error);
+let characters = [];
+request(url, (err, resp, body) => {
+  if (err || resp.statusCode !== 200) console.log(err);
+  else characters = JSON.parse(body).characters;
+  const size = Object.keys(characters).length;
+  const array = Array(size).fill();
+  let data = 0;
+  for (let i = 0; i < size; i++) {
+    request(characters[i], (erro, respo, bodys) => {
+      if (erro || respo.statusCode !== 200) console.log(erro);
+      else {
+        array[i] = JSON.parse(bodys).name;
+        data++;
       }
-      resolve(JSON.parse(body));
+      if (data === size) {
+        for (let j = 0; j < size; j++) {
+          console.log(array[j]);
+        }
+      }
     });
-  });
-}
-
-async function printChars () {
-  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-  try {
-    const response = await promiseRequest(url);
-    const characters = response.characters;
-    for (const character of characters) {
-      const body = await promiseRequest(character);
-      const name = body.name;
-      console.log(name);
-    }
-  } catch (err) {
-    console.error(err);
   }
-}
-
-printChars();
+});
